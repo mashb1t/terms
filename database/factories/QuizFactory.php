f9422b7a4574dd18fbc2f365fa364602f0975dab
Manuel Schmid <manuel.schmid@odt.net>
2021-08-18 23:14:19 +0200

    Add model quiz

---
+++ database/factories/QuizFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Quiz;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class QuizFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Quiz::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->colorName,
+            'description' => $this->faker->text(60),
+            'owner' => User::all()->random()->id
+        ];
+    }
+}
