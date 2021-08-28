4f82f91af3ffd0ba842a3e2d18a9431563f8ad91
Manuel Schmid <manuel.schmid@odt.net>
2021-08-28 14:54:52 +0200

    Add questions table

---
+++ app/Http/Livewire/QuestionsTable.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Question;
+use Illuminate\Database\Eloquent\Model;
+use Mediconesystems\LivewireDatatables\Column;
+use Mediconesystems\LivewireDatatables\NumberColumn;
+
+class QuestionsTable extends AbstractDataTable
+{
+    public $model = Question::class;
+
+    public ?Model $editing;
+
+    public bool $showCreateButton = true;
+
+    public function rules()
+    {
+        return [
+            'editing.question' => 'required',
+            'editing.answer' => 'required',
+        ];
+    }
+
+    public function builder()
+    {
+        $builder = $this->model::query()
+            ->leftJoin('quizzes', 'questions.quiz_id', 'quizzes.id',)
+//            ->leftJoin('slots', 'questions.slot_id', 'slots.id')
+            ->whereOwner(auth()->id());
+
+        return $builder;
+    }
+
+    public function columns()
+    {
+        return [
+            NumberColumn::name('id')->label('ID'),
+            Column::name('quizzes.id')->label(__('# Quiz'))->width(100),
+            Column::name('slot_id')->width(100),
+            Column::name('question')->truncate(50),
+            Column::name('answer')->truncate(50),
+
+            Column::callback(['id', 'question'], function ($id, $name) {
+                return view('datatables.table-actions', [
+                    'id' => $id,
+                    'name' => $name,
+                    'actions' => [
+                        'edit' => true,
+                        'delete' => true,
+                    ],
+                ]);
+            })->label('Actions'),
+        ];
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,6 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <livewire:quizzes-table />
+            <livewire:questions-table />
         </div>
     </div>
 </x-app-layout>
