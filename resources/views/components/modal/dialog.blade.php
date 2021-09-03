60f0b36e0f12a93b9e4ecd79aebd51074da15086
Manuel Schmid <manuel.schmid@odt.net>
2021-09-03 22:55:15 +0200

    Move modal to subfolder

--- resources/views/components/modal/dialog.blade.php
+++ resources/views/components/modal/dialog.blade.php
@@ -1,6 +1,6 @@
 @props(['id' => null, 'maxWidth' => null])
 
-<x-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
+<x-modal.modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
     <div class="px-6 py-4">
         <div class="text-lg">
             {{ $title }}
