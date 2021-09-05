e4fef506c3db2b849d197d0641fffea56250bd6f
Manuel Schmid <manuel.schmid@odt.net>
2021-09-05 17:43:24 +0200

    Add website icon to error template

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -35,6 +35,10 @@
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
+                    <div style="line-height: 1;">
+                        <img src="/images/colorful_logo.svg" class="w-5" />
+                    </div>
+
                     <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
                         @yield('code')
                     </div>
